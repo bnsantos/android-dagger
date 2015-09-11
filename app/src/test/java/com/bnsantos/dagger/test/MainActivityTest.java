145fd5f6b1b3e9afdb70b1e0870d1e3941382cd8
Bruno Santos <bnsantos17@gmail.com>
2015-09-11 15:03:19 -0300

    Test working

--- app/src/test/java/com/bnsantos/dagger/test/MainActivityTest.java
+++ app/src/test/java/com/bnsantos/dagger/test/MainActivityTest.java
@@ -2,11 +2,13 @@ package com.bnsantos.dagger.test;
 
 import android.content.Context;
 import android.os.Build;
+import android.widget.TextView;
 
 import com.bnsantos.dagger.App;
 import com.bnsantos.dagger.BuildConfig;
 import com.bnsantos.dagger.Dagger2Helper;
 import com.bnsantos.dagger.MainActivity;
+import com.bnsantos.dagger.R;
 import com.bnsantos.dagger.test.dagger.TestComponent;
 import com.bnsantos.dagger.test.dagger.TestModule;
 
@@ -38,12 +40,12 @@ public class MainActivityTest {
     @Test
     public void testInitialState(){
         mMainActivity = Robolectric.buildActivity(MainActivity.class).get();
-        //MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
         Assert.assertNotNull(mMainActivity);
-        /*TextView version = (TextView) mMainActivity.findViewById(R.id.version);
-        TextView value = (TextView) mMainActivity.findViewById(R.id.value);
+        TextView version = (TextView) activity.findViewById(R.id.version);
+        TextView value = (TextView) activity.findViewById(R.id.value);
 
         Assert.assertEquals("1", version.getText().toString());
-        Assert.assertEquals("ProviderImpl", value.getText().toString());*/
+        Assert.assertEquals("ProviderImpl", value.getText().toString());
     }
 }
